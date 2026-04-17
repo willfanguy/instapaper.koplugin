50132f1060a344a0c238cb61835543efac592aed
ömer faruk inel <omer.inel@ciellos.com>
2026-04-17 19:36:36 +0300

    v1.2.1

--- _meta.lua
+++ _meta.lua
@@ -3,5 +3,5 @@ return {
     name = "instapaper",
     fullname = _("Instapaper"),
     description = _([[Download and read articles saved in Instapaper.]]),
-    version = "1.2.0",
+    version = "1.2.1",
 }
